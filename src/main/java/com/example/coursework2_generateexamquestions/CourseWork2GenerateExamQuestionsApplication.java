2e92e81c281c15afc2f46d52eefd951ffc02c073
vadim <vadimpoldolin19@gmail.com>
2023-07-28 18:46:02 +0200

    first commit

---
+++ src/main/java/com/example/coursework2_generateexamquestions/CourseWork2GenerateExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursework2_generateexamquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2GenerateExamQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork2GenerateExamQuestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursework2_generateexamquestions/CourseWork2GenerateExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework2_generateexamquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2GenerateExamQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
